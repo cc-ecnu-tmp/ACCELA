67b10039c1aa97acbe852d2b44b9a9cdd0ac382c
Zeyi Xu <mitchell.xu2@gmail.com>
2026-07-11 07:05:54 +0800

    Enable small loop function inlining

--- src/main/java/accela/pass/PassBuilder.java
+++ src/main/java/accela/pass/PassBuilder.java
@@ -25,6 +25,7 @@ import accela.pass.ir.transform.SCCP;
 import accela.pass.ir.transform.SimplifyCFG;
 import accela.pass.ir.transform.SROA;
 import accela.pass.ir.transform.SmallFunctionInliner;
+import accela.pass.ir.transform.SmallLoopInliner;
 import accela.pass.ir.transform.TailRecursionElimination;
 
 /**
@@ -168,6 +169,7 @@ public final class PassBuilder {
     ModulePassManager mpm = new ModulePassManager(instrumentation);
     mpm.addPass(new GlobalConstantPropagation.Pass());
     mpm.addPass(new ModuleToFunctionPassAdaptor(fpm));
+    mpm.addPass(new SmallLoopInliner.Pass());
     mpm.addPass(new ReadNoneCallCSE.Pass());
     mpm.addPass(new SmallFunctionInliner.Pass());
     mpm.addPass(new IPSCCP.Pass());
