f04600915eff9b99a7c4ee17435572414d1dae88
Zeyi Xu <mitchell.xu2@gmail.com>
2026-07-11 11:10:32 +0800

    Inline unique large loop functions

--- src/main/java/accela/pass/ir/transform/SmallLoopInliner.java
+++ src/main/java/accela/pass/ir/transform/SmallLoopInliner.java
@@ -14,8 +14,8 @@ import java.util.Set;
 
 /** Inlines uniquely called, leaf functions containing one small loop. */
 public final class SmallLoopInliner {
-  private static final int MAX_BLOCKS = 12;
-  private static final int MAX_INSTRUCTIONS = 96;
+  private static final int MAX_BLOCKS = 24;
+  private static final int MAX_INSTRUCTIONS = 200;
 
   private SmallLoopInliner() {}
 
